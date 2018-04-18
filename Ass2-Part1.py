b9ae179e4ae63e41e57d01cdccc93a493ff8b426
EthChil <ethanchilderhose@gmail.com>
2018-04-18 12:27:13 -0400

    FInished programs

--- Ass2-Part1.py
+++ Ass2-Part1.py
@@ -1,12 +1,64 @@
 import time
 
+#***********************************************************************#
+#                                                                       #
+#                          PROGRAM HEADER                               #
+#***********************************************************************#
+#***********************************************************************#
+#                                                                       #
+# PROGRAMMER'S NAME:    Ethan Childerhose                               #
+#                                                                       #
+# DATE:                 Wednesday, April 18 2018                        #
+#                                                                       #
+# PROGRAM NAME:         Assignment 3                                    #
+#                                                                       #
+# CLASS:                TEJ-3M1                                         #
+#                                                                       #
+# ASSIGNMENT:           Assignment 2                                    #
+#                                                                       #
+# TEACHER:              Mr. Henrich                                     #
+#                                                                       #
+# DUE DATE:             Wednesday, April 18 2018                        #
+#                                                                       #
+#***********************************************************************#
+#                                                                       #
+# WHAT THE PROGRAM DOES                                                 #
+#                                                                       #
+# This program will encode and decode ROT N ecnryptions                 #
+#                                                                       #
+#***********************************************************************#
+#                                                                       #
+# PROCEDURES                                                            #
+#                                                                       #
+# No procedures                                                         #
+#***********************************************************************#
+#                                                                       #
+# ERROR HANDLING                                                        #
+#                                                                       #
+# Verify input from user to ensure it's numerical or not                #
+#                                                                       #
+#***********************************************************************#
+#                                                                       #
+# PROGRAM LIMITATIONS                                                   #
+#                                                                       #
+# There is no way to change the student's marks after they have been    #
+# inputted.                                                             #
+#***********************************************************************#
+#                                                                       #
+# EXTENSIONS AND IMPROVEMENTS                                           #
+# While the program follows the output, the program could be improved by#
+# allowing the user to change the marks of an already submitted student #
+#***********************************************************************#
+
+
+
 while(True):
     entry = raw_input("Please enter the number of spaces each letter in the alphabet is to move to the right. (1 to 26)")
 
     try:
         offset = int(entry)
 
-        if(offset >= 1 and offset <= 26):
+        if((offset >= 1 and offset <= 26)):
             break
         else:
             print("That is not within the  1 to 26 range")
@@ -22,6 +74,9 @@ word = raw_input("Enter in a word to encrypt or decrypt")
 if word == "END":
     exit(0)
 
+if word == "\"END\"":
+    word = "END"
+
 while True:
     quest = raw_input("Enter 'Y' to encrypt or 'N' to decrypt the entered word")
 
@@ -35,26 +90,49 @@ if("Y" in quest or "y" in quest):
     output = []
 
     for i in word:
+        LOWER = False
+
+        if(i.islower()):
+            LOWER = True
+
+
         i = i.lower()
 
         if (i != " "):
-                if(ord(i) + offset > 122):
-                    output.append(chr((ord(i) + offset) - 26))
+                if(not LOWER):
+                    if (ord(i) + offset > 122):
+                        output.append(chr((ord(i) + offset) - 26).upper())
+                    else:
+                        output.append(chr(ord(i) + offset).upper())
                 else:
-                    output.append(chr(ord(i) + offset))
+                    if(ord(i) + offset > 122):
+                        output.append(chr((ord(i) + offset) - 26))
+                    else:
+                        output.append(chr(ord(i) + offset))
         else:
             output.append(" ")
 else:
     output = []
 
     for i in word:
+        LOWER = False
+
+        if(i.islower()):
+            LOWER = True
+
         i = i.lower()
 
         if(i != " "):
-            if(ord(i) - offset < 97):
-                output.append(chr((ord(i) - offset)+ 26))
+            if (not LOWER):
+                if (ord(i) + offset > 122):
+                    output.append(chr((ord(i) - offset) - 26).upper())
+                else:
+                    output.append(chr(ord(i) - offset).upper())
             else:
-                output.append(chr(ord(i) - offset))
+                if (ord(i) + offset > 122):
+                    output.append(chr((ord(i) - offset) - 26))
+                else:
+                    output.append(chr(ord(i) - offset))
         else:
             output.append(" ")
 
--- Ass2-PartA-XOR.py
+++ Ass2-PartA-XOR.py
@@ -1,22 +1,101 @@
 import time, binascii
 
-while(True):
-    entry = raw_input("Please enter the key to encrypt / decrypt with using a XOR process")
+#***********************************************************************#
+#                                                                       #
+#                          PROGRAM HEADER                               #
+#***********************************************************************#
+#***********************************************************************#
+#                                                                       #
+# PROGRAMMER'S NAME:    Ethan Childerhose                               #
+#                                                                       #
+# DATE:                 Wednesday, April 18 2018                        #
+#                                                                       #
+# PROGRAM NAME:         Assignment 3                                    #
+#                                                                       #
+# CLASS:                TEJ-3M1                                         #
+#                                                                       #
+# ASSIGNMENT:           Assignment 2                                    #
+#                                                                       #
+# TEACHER:              Mr. Henrich                                     #
+#                                                                       #
+# DUE DATE:             Wednesday, April 18 2018                        #
+#                                                                       #
+#***********************************************************************#
+#                                                                       #
+# WHAT THE PROGRAM DOES                                                 #
+#                                                                       #
+# This program will encode and decode ROT N ecnryptions                 #
+#                                                                       #
+#***********************************************************************#
+#                                                                       #
+# PROCEDURES                                                            #
+#                                                                       #
+# No procedures                                                         #
+#***********************************************************************#
+#                                                                       #
+# ERROR HANDLING                                                        #
+#                                                                       #
+# Verify input from user to ensure it's numerical or not                #
+#                                                                       #
+#***********************************************************************#
+#                                                                       #
+# PROGRAM LIMITATIONS                                                   #
+#                                                                       #
+# There is no way to change the student's marks after they have been    #
+# inputted.                                                             #
+#***********************************************************************#
+#                                                                       #
+# EXTENSIONS AND IMPROVEMENTS                                           #
+# While the program follows the output, the program could be improved by#
+# allowing the user to change the marks of an already submitted student #
+#***********************************************************************#
 
-    try:
-        offset = int(entry)
+counter = 0
 
-        break
+def xor(char):
+    binary = "0"+str(bin(ord(char))).split("b")[1]
 
-    except ValueError:
-        print("That is not a number")
+    while(True):
+        if(len(binary) < 8):
+            binary = "0"+binary
+        else:
+            break
 
-    time.sleep(1)
+    #print(binary)
 
+    out = []
+
+    global counter
+
+    #print(key)
+
+    for i in range(len(binary)):
+
+        if(int(key[counter]) != int(binary[i])):
+            out.append("1")
+        else:
+            out.append("0")
+
+        if(counter < len(key)-1):
+            counter += 1
+        else:
+            counter = 0
+
+
+    return "".join(out)
+
+offset = raw_input("Please enter the key to encrypt / decrypt with using a XOR process")
 
 
 if offset == "END":
     exit(0)
+
+enteredKey = []
+
+for i in offset:
+    enteredKey.append("0"+str(bin(ord(i))).split("b")[1])
+
+key = ''.join(enteredKey)
 
 word = raw_input("Enter in a word to encrypt or decrypt")
 
@@ -33,31 +112,15 @@ while True:
     print("not a valid input")
     time.sleep(1)
 
-def xor(key, char):
-    binary = str(bin(ord(char))).split("b")[1]
-    out = []
-    key = str(key)
-    print(key)
-
-    for i in range(len(binary)):
-
-        if(int(key[i]) != int(binary[i])):
-            out.append("1")
-        else:
-            out.append("0")
-
-    return "".join(out)
-
-
-
 
 if("Y" in quest or "y" in quest):
     output = []
 
     for i in word:
-        i.lower()
-
-        output.append(chr(int(xor(offset, i),2)))
+        #print(i)
+        output.append(str(hex(int(xor(i), 2))).split("x")[1])
+        #output.append(int(xor(i), 2))
+        #output.append(xor(i))
 
     print(output)
 
@@ -67,13 +130,10 @@ else:
     output = []
 
     for i in word:
-        i = i.lower()
+        #print(i)
+        output.append(str(hex(int(xor(i), 2))).split("x")[1])
+        #output.append(int(xor(i), 2))
+        #output.append(xor(i))
 
-        if(i != " "):
-            if(ord(i) - offset < 97):
-                output.append(chr((ord(i) - offset)+ 26))
-            else:
-                output.append(chr(ord(i) - offset))
-        else:
-            output.append(" ")
+    print(output)
 
