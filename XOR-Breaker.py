5e03d6ed1ffe99224f0a183ea4547875f3fe1707
EthChil <ethanchilderhose@gmail.com>
2018-04-24 17:19:38 -0400

    changes

--- XOR-Breaker.py
+++ XOR-Breaker.py
@@ -1,7 +1,9 @@
 
 
 def handleIn():
-    cipherText = raw_input("Enter in the ciphertext in HEX seperate with space: ").split()
+    #cipherText = raw_input("Enter in the ciphertext in HEX seperate with space: ").split()
+
+    cipherText = "0a 07 1a 0f 00 1f 0c 0d 10 01 0e".split()
 
     output = []
 
@@ -21,16 +23,22 @@ def handleIn():
 
 cipher = handleIn()
 
-def bruteForce(index, binaryCipher):
-    binaryTest = str(bin(index)).split("b")[1]
-
-    out = []
+def asciiToBin(ascii):
+    binary = bin(ord(ascii)).split("b")[1]
 
     while (True):
-        if (len(binaryTest) < 8):
-            binaryTest = "0" + binaryTest
+        if (len(binary) < 8):
+            binary = "0" + binary
         else:
             break
+
+    return binary
+
+#Will do a XOR given an ascii
+def bruteForce(index, binaryCipher):
+    binaryTest = asciiToBin(chr(index))
+
+    out = []
 
     for i in range(8):
         if(int(binaryTest[i]) != int(binaryCipher[i])):
@@ -41,6 +49,40 @@ def bruteForce(index, binaryCipher):
     return int(''.join(out), 2)
 
 
-for i in range(255):
-    print(chr(bruteForce(i, "00001101")))
-    print(chr(bruteForce(i, )))
+def convertToBase(number, base):
+    if(number / base < 1):
+        return str(number % base)
+    else:
+        return str(number % base) + "," + str(convertToBase(int(number / base), base))
+
+def genKey(index, list):
+    nums = convertToBase(index, len(list)).split(",")
+
+    output = ""
+
+    for i in nums:
+        output = output + chr(lookupList[int(i)])
+
+    return output
+
+
+
+lookupList = [32,
+              65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+              97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110 ,111, 112, 113, 114, 115, 116, 117 ,118, 119, 120, 121, 122]
+
+
+for i in lookupList:
+    print(chr(i)),
+
+for j in cipher:
+    print("STARTING TO DO " + j)
+    for i in lookupList:
+        decoded = bruteForce(i, j)
+
+        if(decoded == 32 or (decoded >= 65 and decoded <= 90) or (decoded >= 97 and decoded <= 122)):
+
+            print(chr(decoded)),
+        else:
+            print("*"),
+
