58632a5e6822e3bced008a06a4ea1bca2361981a
EthChil <ethanchilderhose@gmail.com>
2018-06-12 16:50:18 -0400

    meme

--- XOR-Breaker.py
+++ XOR-Breaker.py
@@ -1,6 +1,6 @@
 import itertools
 
-lookupList = [32,
+lookupList = [46, 44, 33, 63, 34, 32,
               65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
               97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110 ,111, 112, 113, 114, 115, 116, 117 ,118, 119, 120, 121, 122]
 
@@ -15,7 +15,7 @@ commonLetters = ["E","T","A","O","I","N",
 def handleIn():
     #cipherText = raw_input("Enter in the ciphertext in HEX seperate with space: ").split()
 
-    cipherText = "2c 04 02 07 0b 41 03 12 44 2f 0f 06 01 41 07 18 44 24 1a 03 05 0f".split()
+    cipherText = "05 00 01 09 1c 4d 2c 4d 0b 12 00 00 4d 0c 05 0c 0b".split()
     #fuck the police 0d 10 1a 00 45 0d 03 00 59 1b 0a 15 02 06 1c
     #key
 
@@ -129,7 +129,6 @@ def bruteSingleChar(codes, depth):
 
         for b in codes:
             if(len(b) <= depth):
-                found = False
                 break
 
             b = b[depth]
@@ -147,7 +146,6 @@ def bruteSingleChar(codes, depth):
             possible.append(chr(a))
             scores.append(calcWeight(wordStr))
 
-
     # if(len(possible) > 1):
     #     highestScore = max(scores)
     #
@@ -157,6 +155,18 @@ def bruteSingleChar(codes, depth):
 
     return possible
 
+def breakArr(arr, encrypted):
+    arrPointer = 0
+    decrypt = []
+
+    for letter in encrypted:
+        decrypt.append(chr(xorChar(arr[arrPointer], letter)))
+        if(arrPointer == len(arr) - 1):
+            arrPointer = 0
+        else:
+            arrPointer += 1
+
+    return ''.join(decrypt)
 
 def chunks(l, n):
     for i in range(0, len(l), n):
@@ -167,6 +177,48 @@ def goodResult(list):
         if(len(item) == 0):
             return False
     return True
+
+def testString(wordStr):
+    if (checkDiagraph(wordStr) and checkVowel(wordStr)):
+        return True
+    return False
+
+
+def recursiveSolve(keyArr, encrypt):
+    print("KEY LENGTH 2")
+    if(len(keyArr) == 2):
+        for i in keyArr[0]:
+            for k in keyArr[1]:
+                if(testString(breakArr([i, k], encrypt)) and testString(i+k)):
+                    print(breakArr([i, k], encrypt))
+
+    print("KEY LENGTH 3")
+    if (len(keyArr) == 3):
+        for i in keyArr[0]:
+            for j in keyArr[1]:
+                for k in keyArr[2]:
+                    if (testString(breakArr([i, j, k], encrypt)) and testString(i+j+k)):
+                        print(breakArr([i, j, k], encrypt))
+
+    print("KEY LENGTH 4")
+    if (len(keyArr) == 4):
+        for i in keyArr[0]:
+            for j in keyArr[1]:
+                for k in keyArr[2]:
+                    for l in keyArr[3]:
+                        if (testString(breakArr([i, j, k, l], encrypt)) and testString(i + j + k + l)):
+                            print(breakArr([i, j, k, l], encrypt))
+
+    print("KEY LENGTH 5")
+    if (len(keyArr) == 5):
+        for i in keyArr[0]:
+            for j in keyArr[1]:
+                for k in keyArr[2]:
+                    for l in keyArr[3]:
+                        for m in keyArr[4]:
+                    if (testString(breakArr([i, j, k], encrypt)) and testString(i + j + k)):
+                        print(breakArr([i, j, k], encrypt))
+
 
 cipher = handleIn()
 
@@ -183,11 +235,13 @@ for n in range(2, len(cipher), 1):
         result.append(bruteSingleChar(list(chunks(cipher, n)), i))
 
     if(goodResult(result)):
+        print(result)
+        recursiveSolve(result, cipher)
         finalKeyAnswers.append(result)
 
-print(finalKeyAnswers[0])
+#print(finalKeyAnswers)
+print("meme")
 
-print(list(itertools.permutations(finalKeyAnswers[0])))
 
 
 
